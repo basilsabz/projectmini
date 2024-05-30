95ac46c40b57f72e14dbd539ab24951281c7785e
basilsabz <146966244+basilsabz@users.noreply.github.com>
2024-05-30 14:04:23 +0530

    rr

---
+++ templates/firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAMpOREfPKWyOnedRqVgRsfNZhsxL3UVhw",
+    authDomain: "commenttoxy.firebaseapp.com",
+    databaseURL: "https://commenttoxy-default-rtdb.firebaseio.com",
+    projectId: "commenttoxy",
+    storageBucket: "commenttoxy.appspot.com",
+    messagingSenderId: "824521023168",
+    appId: "1:824521023168:web:b9ed9ac64ac8ede9149725",
+    measurementId: "G-HVQVD307FE"
+  };
